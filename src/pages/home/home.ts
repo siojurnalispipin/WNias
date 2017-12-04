465cfacd2edb2d7734be79a37f99b744e3603a69
siojurnalispipin <siojurnalispipin@hotmail.com>
2017-12-05 01:13:22 +0700

    edit homepage jadi bentuk pinterest

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,51 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ViewController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  menuIsHidden: boolean = false;
+  estateProperty = {
+    image: 'assets/img/card/advance-card-map-paris.png'
+  }
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+  public viewController: ViewController) {
 
+  }
+  ionViewWillEnter() {
+    // Part 1:
+    this.viewController.showBackButton(false);
+  }
+  ionViewDidLoad() {
+    this.posts = [
+      {
+        description: 'Pantai Sorake Nias',
+        image: 'assets/imgs/wisata/w1.jpg'
+      },
+      {
+        description: 'Pantai Lagundri',
+        image: 'assets/imgs/wisata/w2.jpg'
+      },
+      {
+        description: 'Pantai Gawu Soyo',
+        image: 'assets/imgs/wisata/w3.jpg'
+      },
+      {
+        description: 'Budaya tarian Nias',
+        image: 'assets/imgs/wisata/w4.jpg'
+      },
+      {
+        description: 'Lompat batu Nias',
+        image: 'assets/imgs/wisata/w5.jpg'
+      },
+      {
+        description: 'Wisata Tebing Putih Nias',
+        image: 'assets/imgs/wisata/w6.jpg'
+      }
+    ];
   }
 
 }
