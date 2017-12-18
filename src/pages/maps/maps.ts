1b5a202bcdda7d37d2fbc70630ce9e9f761f1b3a
siojurnalispipin <siojurnalispipin@hotmail.com>
2017-12-19 00:33:37 +0700

    Map potition

--- src/pages/maps/maps.ts
+++ src/pages/maps/maps.ts
@@ -1,6 +1,6 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { IonicPage, NavController} from 'ionic-angular';
-import { Geolocation } from '@ionic-native/geolocation';
+import { Geolocation, GeolocationOptions, Geoposition, PositionError } from '@ionic-native/geolocation';
 
 declare var google;
 
@@ -11,71 +11,126 @@ declare var google;
 })
 export class MapsPage {
 
+  // Add two members for options and current position
+  options : GeolocationOptions;
+  currentPos : Geoposition;
+
+  // member variables of type Array to hold nearby places
+  places : Array<any> ; 
+
+  //add two variables
   @ViewChild('map') mapElement: ElementRef;
   map: any;
-  start = 'chicago, il';
-  end = 'chicago, il';
-  directionsService = new google.maps.DirectionsService;
-  directionsDisplay = new google.maps.DirectionsRenderer;
 
   constructor(public navCtrl: NavController,
-    public geolocation : Geolocation
+    private geolocation : Geolocation
   ) {
 
   }
 
   ionViewDidLoad(){
-    this.initMap();
+    this.getUserPosition();
   }
 
-  initMap() {
-    this.map = new google.maps.Map(this.mapElement.nativeElement, {
-      zoom: 17,
-      center: {lat: 1.284692, lng: 97.622924
+  //Add a method getUserPosition
+  getUserPosition(){
+    this.options = {
+    enableHighAccuracy : false
+    };
+    this.geolocation.getCurrentPosition(this.options).then((pos : Geoposition) => {
+
+        this.currentPos = pos;     
+
+        console.log(pos);
+        this.addMap(pos.coords.latitude,pos.coords.longitude);
+
+    },(err : PositionError)=>{
+        console.log("error : " + err.message);
+    ;
+    })
+}
+  //this method to create a map
+  addMap(lat,long){
+    
+        let latLng = new google.maps.LatLng(lat, long);
+    
+        let mapOptions = {
+        center: latLng,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+    
+        this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+    
+        this.getRestaurants(latLng).then((results : Array<any>)=>{
+            this.places = results;
+            for(let i = 0 ;i < results.length ; i++)
+            {
+                this.createMarker(results[i]);
+            }
+        },(status)=>console.log(status));
+    
+        this.addMarker();
+    
+    }
+
+    //this method to add a marker
+    addMarker(){
+      
+          let marker = new google.maps.Marker({
+          map: this.map,
+          animation: google.maps.Animation.DROP,
+          position: this.map.getCenter()
+          });
+      
+          let content = "<p>This is your current position !</p>";          
+          let infoWindow = new google.maps.InfoWindow({
+          content: content
+          });
+      
+          google.maps.event.addListener(marker, 'click', () => {
+          infoWindow.open(this.map, marker);
+          });
+      
       }
-    });
 
-    this.directionsDisplay.setMap(this.map);
-  }
 
-  calculateAndDisplayRoute() {
-    this.directionsService.route({
-      origin: this.start,
-      destination: this.end,
-      travelMode: 'DRIVING'
-    }, (response, status) => {
-      if (status === 'OK') {
-        this.directionsDisplay.setDirections(response);
-      } else {
-        window.alert('Directions request failed due to ' + status);
+      //https://www.techiediaries.com/ionic-geolocation-google-maps-places-api-part-2/
+      //a getRestaurants() method to get the list of nearby restaurants
+      getRestaurants(latLng)
+      {
+          var service = new google.maps.places.PlacesService(this.map);
+          let request = {
+              location : latLng,
+              radius : 8047 ,
+              types: ["restaurant"]
+          };
+          return new Promise((resolve,reject)=>{
+              service.nearbySearch(request,function(results,status){
+                  if(status === google.maps.places.PlacesServiceStatus.OK)
+                  {
+                      resolve(results);    
+                  }else
+                  {
+                      reject(status);
+                  }
+      
+              }); 
+          });
+      
       }
-    });
-  }
+      
+      //a createMarker() method
+      createMarker(place)
+      {
+          let marker = new google.maps.Marker({
+          map: this.map,
+          animation: google.maps.Animation.DROP,
+          position: place.geometry.location
+          });   
+      }   
 
-  addMarker(){
-    
-     let marker = new google.maps.Marker({
-       map: this.map,
-       animation: google.maps.Animation.DROP,
-       position: this.map.getCenter()
-     });
-    
-     let content = "<h4>Information!</h4>";         
-    
-     this.addInfoWindow(marker, content);
-    
-   }
+      
 
-   addInfoWindow(marker, content){
-    
-     let infoWindow = new google.maps.InfoWindow({
-       content: content
-     });
-    
-     google.maps.event.addListener(marker, 'click', () => {
-       infoWindow.open(this.map, marker);
-     });
-    
-   }
 
 }
